52491a516d990cff670617cfbd43e2a28ef6aac5
ahmedsat <ahmedsat303@gmail.com>
2022-08-30 08:47:29 +0000

    fixes

--- routes/main.go
+++ routes/main.go
@@ -1,14 +1,11 @@
 package routes
 
 import (
-	"github.com/ahmedsat/sat-store/controllers"
 	"github.com/gin-gonic/gin"
 )
 
 func GetRoutes(r *gin.Engine) {
 	v1:=r.Group("/api/v1")
-	v1.GET("/products", controllers.GetAllProducts)
-	v1.GET("/products/:id", controllers.GetOneProduct)
-	v1.POST("/products", controllers.AddProduct)
+	productRoutes(v1)
 }
 // services
---
+++ routes/products-router.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/ahmedsat/sat-store/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func productRoutes(rg *gin.RouterGroup){
+	product:=rg.Group("/products")
+
+	product.GET("/", controllers.GetAllProducts)
+	product.GET("/:id", controllers.GetOneProduct)
+	product.POST("/", controllers.AddProduct)
+}
