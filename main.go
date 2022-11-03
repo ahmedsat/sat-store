a3fa354093740f9389ad1600b60ee6401513da2e
ahmedsat <ahmedsat303@gmail.com>
2022-11-03 13:11:44 +0000

    clean

--- main.go
+++ main.go
@@ -1,62 +1,7 @@
 package main
 
-import (
-	"github.com/ahmedsat/sat-store/database"
-	"github.com/gin-gonic/gin"
-	// _ "github.com/gwenn/gosqlite"
-)
-
-//	type user struct {
-//		id        int
-//		name      string
-//		username  string
-//		email     string
-//		password  string
-//		privilege string
-//	}
-
 func main() {
 
-	// db, err := sql.Open("sqlite3", "database.db")
-	database.Connect("database.db")
-	database.Migrate()
-
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// defer db.Close()
-
-	// controllers.DB = db
-
-	r := gin.Default()
-
-	// routes.GetRoutes(r)
-
-	r.Run("localhost:8080")
+	println("hi")
 
 }
-
-// func allUsers() ([]user, error) {
-// 	var users []user
-
-// 	rows, err := db.Query(`SELECT * FROM users`)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 		return nil, err
-// 	}
-// 	defer rows.Close()
-
-// 	for rows.Next() {
-// 		var u user
-// 		if err = rows.Scan(&u.id, &u.name, &u.username, &u.email, &u.password, &u.privilege); err != nil {
-// 			return nil, err
-// 		}
-// 		users = append(users, u)
-// 	}
-
-// 	if err := rows.Err(); err != nil {
-// 		return nil, err
-// 	}
-// 	return users, nil
-// }
